a709226e8a5e26ace88d4dc895b6074519e8e907
Jpegx100 <jpegx100@gmail.com>
2018-11-27 16:43:49 -0300

    Melhoria das caracteristicas das mensagens no classificador

--- classificador/nltk_classifier.py
+++ classificador/nltk_classifier.py
@@ -1,13 +1,6 @@
 import nltk
 import pickle
 
-def remove_stop_words(sentence):
-    stop_words = nltk.corpus.stopwords.words('portuguese')
-    sent = sentence
-    for sw in stop_words:
-        sent = sent.replace(sw, '')
-    return sent
-
 def get_data(dataset_path='conversas.txt'):
     results = list()
 
@@ -18,9 +11,10 @@ def get_data(dataset_path='conversas.txt'):
     for line in lines:
         splited_line = line.split(',')
         sentence = ','.join(splited_line[:-2])
-        results.append(
-            (remove_stop_words(sentence), int(splited_line[-2]))
-        )
+        try:
+            results.append((sentence, int(splited_line[-2])))
+        except:
+            pass
 
     return results
 
@@ -28,15 +22,52 @@ def get_features(post, postagger):
     features = {}
     classes = []
     words = nltk.word_tokenize(post)
+    q_words_count = 0
+    err_words_count = 0
+    ag_words_count = 0
+    saud_words_count = 0
+    neg_words_count = 0
+    conf_words_count = 0
+    q_words = ['que', 'quem', 'pode', 'posso', 'onde', 'como', 'q', 'porque', 'pq', 'qual']
+    err_words = ['problema', 'erro', 'travando', 'funciona', 'funcionando', 'abre', 'não', 'n', 'trava', 'abrindo', 'abriu', 'abrir', 'nao']
+    ag_words = ['obrigada', 'obrigado', 'obg']
+    saud_words = ['dia', 'tarde', 'noite', 'bom', 'boa', 'oi', 'ola', 'olá', 'oii']
+    neg_words = ['não', 'nao', 'n']
+    conf_words = ['é', 'sim', 'certo', 'bom', 'pronto', 'ok', 'bem', 'beleza', 'blz', 'sei', 'entendo', 'entendido', 'entendi', 'já', 'ja']
     for word in words:
         classe = postagger.classify({'text': word})
-        features['contains({})'.format(word.lower())] = True
         classes.append(classe)
+        if word in q_words:
+            q_words_count = q_words_count + 1
+        if word in err_words:
+            err_words_count = err_words_count + 1
+        if word in ag_words:
+            ag_words_count = ag_words_count + 1
+        if word in saud_words:
+            saud_words_count = saud_words_count + 1
+        if word in neg_words:
+            neg_words_count = neg_words_count + 1
+        if word in conf_words:
+            conf_words_count = conf_words_count + 1
+
     features['pattern'] = ' '.join(classes)
+    features['num_words'] = len(words)
+    features['question_sign'] = '?' in post
+    features['err_words'] = err_words_count
+    features['q_words'] = q_words_count
+    features['ag_words'] = ag_words_count
+    features['saud_words'] = saud_words_count
+    features['neg_words'] = neg_words_count
+    features['conf_words'] = conf_words_count
+
+    if len(words) > 0:
+        features['avg_word_size'] = len(post) / len(words)
+    else:
+        features['avg_word_size'] = 0
+    
     return features
 
 postagger = pickle.load(open('postagger.pickle', 'rb'))
-
 dataset = get_data()
 fSets = [(get_features(text, postagger), label) for (text, label) in dataset]
 size = int(len(fSets) * 0.1)
@@ -47,3 +78,15 @@ print(nltk.classify.accuracy(c1, testSet))
 f = open('classifier.pickle', 'wb')
 pickle.dump(c1, f)
 f.close()
+
+classifier_me = nltk.MaxentClassifier.train(trainSet, max_iter=20)
+acuracia = nltk.classify.accuracy(classifier_me, testSet)
+print("Acuracia MaxEnt: "+str(acuracia))
+# PROBLEMA,1
+# AGRADECIMENTO,2
+# SAUDACAO,3
+# OUTROS,4
+# DUVIDA,5
+# NEGACAO,6
+# CONFIRMACAO,7
+# INFORMACAO,8
